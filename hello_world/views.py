611dd0a2e7b9bd4dfad606e636e80b7a5f7563d3
Tyrel Morant <tyrel_m@hotmail.co.uk>
2024-02-21 13:57:40 +0000

    added new request method in the view.py section

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,3 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("hello_world!")
+
+if request.method == "POST":
+       return HttpResponse("You must have POSTed something")
+else: 
+       return HttpResponse(request.method)
